ca06d6373542d3411c36ddb4f34a19685af08e40
Karlo Domjanić <karlo.domjanic@sigit.hr>
2023-04-21 19:38:37 +0200

    small fix

--- script.js
+++ script.js
@@ -14,39 +14,52 @@ const overlay = document.querySelector('.overlay')
 const btnOverview = document.querySelector('.btn-overview')
 const btnInternal = document.querySelector('.btn-internal')
 const btnSurface = document.querySelector('.btn-surface')
+const btnNaviOverview = document.querySelector('.btn-navi-overview')
+const btnNaviInternal = document.querySelector('.btn-navi-structure')
+const btnNaviSurface = document.querySelector('.btn-navi-surface')
+
 
 function openOverview() {
-    btnOverview.classList.add('active')
+    btnOverview.classList.add('active');
+    btnNaviOverview.classList.add('active-tab');
     imageOverview.classList.remove('hidden');
     textOverview.classList.remove('hidden');
-    btnInternal.classList.remove('active')
+    btnInternal.classList.remove('active');
+    btnNaviInternal.classList.remove('active-tab');
     imageInternal.classList.add('hidden');
     textInternal.classList.add('hidden');
-    btnSurface.classList.remove('active')
+    btnSurface.classList.remove('active');
+    btnNaviSurface.classList.remove('active-tab');
     imageSurface.classList.add('hidden');
     textSurface.classList.add('hidden')
 }
 
 function openInternalStructure() {
     btnOverview.classList.remove('active');
+    btnNaviOverview.classList.remove('active-tab');
     imageOverview.classList.add('hidden');
     textOverview.classList.add('hidden');
     btnInternal.classList.add('active');
+    btnNaviInternal.classList.add('active-tab');
     imageInternal.classList.remove('hidden');
     textInternal.classList.remove('hidden');
     btnSurface.classList.remove('active');
+    btnNaviSurface.classList.remove('active-tab');
     imageSurface.classList.add('hidden');
     textSurface.classList.add('hidden')
 }
 
 function openSurfaceGeology(){
     btnOverview.classList.remove('active');
+    btnNaviOverview.classList.remove('active-tab');
     imageOverview.classList.remove('hidden');
     textOverview.classList.add('hidden');
     btnInternal.classList.remove('active');
+    btnNaviInternal.classList.remove('active-tab');
     imageInternal.classList.add('hidden');
     textInternal.classList.add('hidden');
     btnSurface.classList.add('active');
+    btnNaviSurface.classList.add('active-tab');
     imageSurface.classList.remove('hidden');
     textSurface.classList.remove('hidden')
 }
